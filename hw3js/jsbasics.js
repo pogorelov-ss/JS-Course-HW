cfd5c378ebd0d4f6d95b8b0317baa9dc173b5244
pogorelov-ss <pogorelov.ss@gmail.com>
2013-11-21 09:33:17 +0200

    new getFriends func

--- hw3js/jsbasics.js
+++ hw3js/jsbasics.js
@@ -6,72 +6,72 @@ var HW3 = HW3 || {
         },
         s: 'f'
     },
-    people: [{
-        id: 1,
-        name: 'Brad',
-        friends: [2, 5, 6]
-    }, {
-        id: 2,
-        name: 'John',
-        friends: [1, 3]
-    }, {
-        id: 3,
-        name: 'Tom',
-        friends: [2, 5]
-    }, {
-        id: 4,
-        name: 'Fil',
-        friends: null
-    }, {
-        id: 5,
-        name: 'John',
-        friends: [1, 3]
-    }, {
-        id: 6,
-        name: 'Jim',
-        friends: [1]
-    }],
-    getObject: function(path, obj) {
+    people: [
+        {
+            id: 1,
+            name: 'Brad',
+            friends: [2, 5, 6]
+        },
+        {
+            id: 2,
+            name: 'John',
+            friends: [1, 3,55]
+        },
+        {
+            id: 3,
+            name: 'Tom',
+            friends: [2, 5]
+        },
+        {
+            id: 4,
+            name: 'Fil',
+            friends: null
+        },
+        {
+            id: 5,
+            name: 'John',
+            friends: [1, 3]
+        },
+        {
+            id: 6,
+            name: 'Jim',
+            friends: [1]
+        }
+    ],
+    getObject: function (path, obj) {
         var parts = path.split('.');
         for (var i = 0; i < parts.length; i++) {
             obj = obj[parts[i]];
-        };
+        }
         return obj;
     },
     //наверное не оптимально по производительности ... содрать из jquery extend
-    deepCopy: function(obj) {
+    deepCopy: function (obj) {
 
         return JSON.parse(JSON.stringify(obj));
     },
-    //надо рефакторить ... ибо криво
-    getFriends: function(id) {
-        var persons = [];
-
-        function myFriends(id, people) {
-            var persons = {};
-            for (var i = 0; i < people.length; i++) {
-                if (~people[i]['id'].toString().indexOf(id)) {
-                    persons = people[i];
+    findFriends:function findF (){
+        var id = [];
+        var friends = [];
+        id = id.concat(arguments[0]);
+        console.log(id);
+            for (var i=0; i < this.people.length; i++){
+                if (~id.indexOf(this.people[i].id)){
+                    friends.push(this.people[i]);
                 }
-            }
-            return persons;
-        };
-        for (var i = 0; i < this.people.length; i++) {
-            if (~this.people[i]['id'].toString().indexOf(id)) {
-                for (var ii = 0; ii < this.people[i]['friends'].length; ii++) {
-                    persons.push(myFriends(this.people[i]['friends'][ii], this.people));
-                };
-            }
         }
-        return persons;
+        return friends;
+    },
+    getFriends:function(id){
+        return HW3.findFriends(HW3.findFriends(id)[0].friends);
     }
 
 };
 
-console.log('t1');
-console.log(HW3.getObject('a.d', HW3.myObj1));
-console.log(HW3.getObject('a', 1));
-console.log('t2');
-console.log(HW3.deepCopy(HW3.myObj1))
-console.log('t3');
+//console.log('t1');
+//console.log(HW3.getObject('a.d', HW3.myObj1));
+//console.log(HW3.getObject('a', 1));
+//console.log('t2');
+//console.log(HW3.deepCopy(HW3.myObj1));
+//console.log('t3');
 console.log(HW3.getFriends(2));
