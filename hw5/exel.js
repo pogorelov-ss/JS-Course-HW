391b3d9f63fb526d0c1958bd1774414a28d99573
pogorelov-ss <pogorelov.ss@gmail.com>
2013-12-12 09:54:39 +0200

    hw5 task2 99%

--- hw5/exel.js
+++ hw5/exel.js
@@ -11,25 +11,50 @@ var HW5 = HW5 || {
     columns:1,
     Cursor:{row:1,column:1},
     selectCell: function (element) {
-        if(this.selectedCell!=null){
-            this.selectedCell.className = 'cell';
-        }
+        this.deselectCell();
+        this.deselectRow();
+        this.deselectTable();
         this.selectedCell = element;
         this.selectedRow = this.selectedCell.parentNode;
         console.log(this.selectedRow.rowIndex);
-        element.className = 'selected';
+        element.classList.toggle('selected');
     },
-    drawCursor:function(){},
+    selectRow:function(element){
+        this.deselectCell();
+        this.deselectTable();
+        this.deselectRow();
+        element.classList.toggle('selected');
+        this.selectedRow = element;
+    },
+    selectTable:function(){
+        this.deselectCell();
+        this.deselectRow();
+        this.tableElement.firstElementChild.classList.toggle('selected');
+    },
+    deselectCell:function(){
+        if(this.selectedCell!=null){
+            this.selectedCell.className = 'cell';
+        }
+        //this.selectedCell = null;
+    },
+    deselectRow:function(){
+        if(this.selectedRow!=null){
+            this.selectedRow.className = 'row';
+        }
+        //this.selectedRow = null;
+    },
+    deselectTable:function(){
+        this.tableElement.firstElementChild.className = 'tbody';
+    },
     init: function (selector) {
         this.tableElement = document.querySelector(selector);
         this.keyHandler.add();
         this.tableElement.onclick = this.mouseHandler;//шансов что на нашу табличку еще ктонить повесит онклик немного?
-        this.selectedRow = this.tableElement.querySelector('#firstRow');
-        this.selectedCell = this.tableElement.querySelector('#firstCell');
-
+        this.selectCell(this.tableElement.firstElementChild.firstElementChild.firstElementChild);
     },
     addRow:function(){
-        var row = HW5.tableElement.insertRow(-1);//.insertCell(-1).className='cell';
+        var row = HW5.tableElement.insertRow(-1);
+        row.className = 'row';//.insertCell(-1).className='cell';
         for (var i = 0; i < this.columns; i++) {
             row.insertCell(-1).className='cell';
         }
@@ -54,10 +79,41 @@ var HW5 = HW5 || {
         }
         this.columns--;
     },
-    moveCursorLR:function(direction){//+-1
-
+    moveCursorLR:function(direction){//+-n
+        this.deselectRow();
+        var CellIndex = this.selectedCell.cellIndex,
+            nextCell;
+        if (this.selectedCell.classList.contains('selected')){
+            nextCell = this.selectedRow.children[CellIndex+direction];
+        }
+        else {
+            nextCell = this.selectedRow.children[CellIndex];
+        }
+        if (nextCell) this.selectCell(nextCell);
+        else this.selectRow(this.selectedRow);
     },
-    moveCursorUD:function(direction){//+-1
+    moveCursorUD:function(direction){//+-n
+        var RowIndex = this.selectedRow.rowIndex,
+            nextRow;
+        if (this.tableElement.firstElementChild.classList.contains('selected')){
+            this.selectRow(this.selectedRow);
+            //this.selectedCell=this.selectedRow.firstElementChild;
+            return
+        }
+        nextRow = this.tableElement.firstElementChild.children[RowIndex+direction];
+        if (nextRow){
+            if (this.selectedRow.classList.contains('selected')){
+                this.selectRow(nextRow);
+            }
+            else{
+                this.selectedRow = nextRow;
+                this.selectCell(nextRow.children[this.selectedCell.cellIndex])
+            }
+        }
+        else{
+            this.selectTable();
+        }
+
 
     },
     keyHandler: {//тут я немного извратился с изменением объекта .. и надо б поменять добавление обработчика
@@ -91,12 +147,16 @@ var HW5 = HW5 || {
                     }
                         break;
                     case 37:console.log('<');
+                        HW5.moveCursorLR(-1);
                         break;
                     case 38:console.log('up');
+                        HW5.moveCursorUD(-1);
                         break;
                     case 39:console.log('>');
+                        HW5.moveCursorLR(1);
                         break;
                     case 40:console.log('down');
+                        HW5.moveCursorUD(1);
                         break;
                     default:
                         console.log('non');
